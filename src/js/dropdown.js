687938151e6ce1c73b8fc8f0e30a5377ad779096
Ascarbek <ascarbek@gmail.com>
2015-12-02 17:51:37 +0600

    typeahead add/remove selection

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -4,6 +4,7 @@
 (function(){
     'use strict';
 
+    var uid = 0;
     angular
         .module('azSuggestBox')
         .directive('sbDropDown', [function(){
@@ -23,7 +24,6 @@
                         }
                     });
 
-
                     scope.$watchCollection('list',function(){
                         for(var b=0; b<blocks.length; b++){
                             blocks[b].clone[1].outerHTML = '';
@@ -33,15 +33,55 @@
                         blocks = [];
 
                         for(var i=0; i<scope.list.length; i++) {
-                            var currentModel = scope.list[i];
+
+                            var currentItem = scope.list[i];
                             var newScope = scope.$new();
-                            newScope[listAlias] = currentModel;
+                            newScope[listAlias] = currentItem;
                             transclude(newScope, function (clone, scope) {
+                                scope.hidden = false;
+                                scope.hide = function(){
+                                    angular.element(clone).addClass('ng-hide');
+                                    scope.hidden = true;
+                                };
+                                scope.show = function(){
+                                    angular.element(clone).removeClass('ng-hide');
+                                    scope.hidden = false;
+                                };
+                                scope.highlight = function(){
+                                    angular.element(clone).addClass('sb-list-item-highlight');
+                                };
+                                scope.unHighlight = function(){
+                                    angular.element(clone).removeClass('sb-list-item-highlight');
+                                };
+
                                 blocks.push({scope: scope, clone: clone});
+
                                 element.append(clone);
+
+                                clone.on('click', function(){
+                                    scope.model.push(scope[listAlias]);
+                                    scope.$apply();
+                                });
                             });
                         }
                     });
+
+                    scope.hideListItem = function(itemId){
+                        blocks[itemId].scope.hide();
+                    };
+
+                    scope.showListItem = function(itemId){
+                        blocks[itemId].scope.show();
+                    };
+
+                    scope.highlightedItem = -1;
+                    scope.highlightListItem = function(itemId){
+                        if(scope.highlightedItem > -1) {
+                            blocks[scope.highlightedItem].scope.unHighlight();
+                        }
+                        blocks[itemId].scope.highlight();
+                        scope.highlightedItem = itemId;
+                    }
                 }
             }
         }])
